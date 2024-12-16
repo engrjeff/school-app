5084e29b53cfe66f55e0b6aecd4f5305db84049a
engrjeff <engineerjeff15@gmail.com>
2024-12-16 08:41:28 +0800

    category table changes

---
+++ prisma/migrations/20241216004113_add_unique_constrant_and_store_index_to_category/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[storeId,name]` on the table `Category` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE INDEX "Category_storeId_idx" ON "Category"("storeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_storeId_name_key" ON "Category"("storeId", "name");
