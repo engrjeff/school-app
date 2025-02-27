d1276475e544f1741c59d9de653883a70dbb91ad
engrjeff <engineerjeff15@gmail.com>
2025-02-28 07:00:25 +0800

    make sure subjects with the same code and/or name cannot exist inside a course

--- features/subjects/actions.ts
+++ features/subjects/actions.ts
@@ -11,6 +11,35 @@ export const updateSubject = adminActionClient
   .metadata({ actionName: "updateSubject" })
   .schema(updateSubjectSchema)
   .action(async ({ parsedInput }) => {
+    const existingCode = await prisma.subject.findFirst({
+      where: {
+        OR: [
+          {
+            code: parsedInput.code,
+            id: {
+              not: parsedInput.id,
+            },
+            courseId: parsedInput.courseId,
+          },
+          {
+            title: {
+              equals: parsedInput.title,
+              mode: "insensitive",
+            },
+            id: {
+              not: parsedInput.id,
+            },
+            courseId: parsedInput.courseId,
+          },
+        ],
+      },
+    })
+
+    if (existingCode)
+      throw new Error(
+        `The given code/title is already in use by another subject in this course.`
+      )
+
     const subject = await prisma.subject.update({
       where: { id: parsedInput.id },
       data: {
